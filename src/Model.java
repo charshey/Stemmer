6d9719d20e4f6691d209300922a0837f985495fc
Clare Harshey <charshey@indiana.edu>
2018-05-01 23:36:33 -0400

    structure

---
+++ src/Model.java
@@ -0,0 +1,16 @@
+public class Model {
+
+    // language data
+    // trie
+
+    // constructor: take in parameters for algorithm
+
+    // read freq profile
+
+    // construct trie[s]
+
+    // return list of stems
+
+    // write model to save
+
+}
---
+++ src/Trie.java
@@ -0,0 +1,5 @@
+public class Trie {
+
+    // internal class node
+
+}
